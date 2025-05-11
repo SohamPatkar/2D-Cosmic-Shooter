2782c30bf11fbffd0d33f6f22dc4a34b3c6db64a
sohamPatkar <spatkar398@gmail.com>
2025-05-11 13:17:02 +0530

    Generic Pool Script changes with create Item

--- Assets/Scripts/Utilities/GenericObjectPool.cs
+++ Assets/Scripts/Utilities/GenericObjectPool.cs
@@ -3,42 +3,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GenericObjectPool<T> where T : class
+namespace CosmicCuration.Utilities
 {
-    private List<PooledItem<T>> pooledItems = new List<PooledItem<T>>();
-
-    public T GetItem()
+    public class GenericObjectPool<T> where T : class
     {
-        if (pooledItems.Count > 0)
-        {
-            PooledItem<T> pooledItem = pooledItems.Find(item => !item.isUsed);
+        private List<PooledItem<T>> pooledItems = new List<PooledItem<T>>();
 
-            if (pooledItem != null)
+        protected T GetItem()
+        {
+            if (pooledItems.Count > 0)
             {
-                pooledItem.isUsed = true;
-                return pooledItem.Item;
+                PooledItem<T> pooledItem = pooledItems.Find(item => !item.isUsed);
+
+                if (pooledItem != null)
+                {
+                    pooledItem.isUsed = true;
+                    return pooledItem.Item;
+                }
             }
+
+            return CreatePooledItem();
         }
 
-        return CreatePooledItem();
+        private T CreatePooledItem()
+        {
+            PooledItem<T> pooledItem = new PooledItem<T>();
+            pooledItem.Item = CreateItem();
+            pooledItem.isUsed = true;
+            pooledItems.Add(pooledItem);
+            return pooledItem.Item;
+        }
+
+        protected virtual T CreateItem()
+        {
+            throw new NotImplementedException("Please use create Item");
+        }
+
+        public class PooledItem<T>
+        {
+            public T Item;
+            public bool isUsed;
+        }
     }
 
-    private T CreatePooledItem()
-    {
-        PooledItem<T> pooledItem = new PooledItem<T>();
-        pooledItem.Item = CreateItem();
-        pooledItem.isUsed = true;
-        return pooledItem.Item;
-    }
-
-    public T CreateItem()
-    {
-        throw new NotImplementedException("Please use create Item");
-    }
-
-    public class PooledItem<T>
-    {
-        public T Item;
-        public bool isUsed;
-    }
 }
