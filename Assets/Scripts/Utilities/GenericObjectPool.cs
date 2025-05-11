f9ab4f7fbf3d6cb11f30ccd1c6446eec00d1bd1c
sohamPatkar <spatkar398@gmail.com>
2025-05-11 12:55:02 +0530

    Generic Object Pool Script

---
+++ Assets/Scripts/Utilities/GenericObjectPool.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenericObjectPool<T> where T : class
+{
+    private List<PooledItem<T>> pooledItems = new List<PooledItem<T>>();
+
+    public T GetItem()
+    {
+        if (pooledItems.Count > 0)
+        {
+            PooledItem<T> pooledItem = pooledItems.Find(item => !item.isUsed);
+
+            if (pooledItem != null)
+            {
+                pooledItem.isUsed = true;
+                return pooledItem.Item;
+            }
+        }
+    }
+
+    public class PooledItem<T>
+    {
+        public T Item;
+        public bool isUsed;
+    }
+}
